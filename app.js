2e04ee9bef3ed339d2b76cf367b3b5084d98fb1f
Yazh24 <aurmc2024@gmail.com>
2025-03-28 03:27:05 +0530

    Added CI Pipeline with GituP Actions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const name = "CodeTest";
 
 app.get("/", (req, res) => {
-    const message = `Welcome to DevOps Easy  from ${name}! - Modified`
+    const message = `Welcome to DevOps Easy  from RMC - Modified`
     res.send(message);
 })
 
@@ -12,4 +12,4 @@ const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
-module.exports = {app,server,name }
+module.exports = {app,server}
